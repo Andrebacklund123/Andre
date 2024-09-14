723da1a269e5242ab4beb43c76806e0a2a484eca
Andrebacklund123 <backlund.andre@hotmail.com>
2024-09-14 12:11:26 +0200

    Update  service-worker.js

--- Meny Ullevi/ service-worker.js
+++ Meny Ullevi/ service-worker.js
@@ -1,22 +1,13 @@
-// Installera service workern och cacha resurser
 self.addEventListener('install', function(event) {
   event.waitUntil(
     caches.open('ullevi-tk-cache').then(function(cache) {
       return cache.addAll([
         '/index.html',
         '/manifest.json',
-        '/mnt/data/ullevitk_logo.png'
-        // Lägg till fler resurser här som CSS, bilder etc.
+        '/ullevitk_logo.png',
+        '/android-chrome-192x192.png',
+        '/apple-touch-icon.png'
       ]);
-    })
-  );
-});
-
-// Hantera nätverksförfrågningar och använd cache
-self.addEventListener('fetch', function(event) {
-  event.respondWith(
-    caches.match(event.request).then(function(response) {
-      return response || fetch(event.request);
     })
   );
 });
